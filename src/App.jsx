b255cd67403854c5d7ae215457781e9de183d6d0
Suryakant Thombre <suryathombre000@gmail.com>
2024-02-10 00:26:48 +0530

    extracted a reusabke copone t for powerbi-report

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,52 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import DemoApp from './Powerbi'
+import { models, Report, Embed, service, Page } from 'powerbi-client';
+import { sampleReportUrl } from '../public/constants.js';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isEmbedded, setIsEmbedded] = useState(false);
+
+  const [sampleReportConfig, setReportConfig] = useState({
+    type: 'report',
+    embedUrl: undefined,
+    tokenType: models.TokenType.Embed,
+    accessToken: undefined,
+    settings: undefined,
+});
+
+const embedReport = async () => {
+  console.log('Embed Report clicked');
+  const reportConfigResponse = await fetch(sampleReportUrl);
+
+  if (reportConfigResponse === null) {
+      return;
+  }
+
+  if (!reportConfigResponse?.ok) {
+      console.error(`Failed to fetch config for report. Status: ${ reportConfigResponse.status } ${ reportConfigResponse.statusText }`);
+      return;
+  }
+
+  const reportConfig = await reportConfigResponse.json();
+
+  setReportConfig({
+      ...sampleReportConfig,
+      embedUrl: reportConfig.EmbedUrl,
+      accessToken: reportConfig.EmbedToken.Token
+  });
+  setIsEmbedded(true);
+
+  // setMessage('Use the buttons above to interact with the report using Power BI Client APIs.');
+};
 
   return (
     <>
       <h1>Here</h1>
-      <DemoApp />
+      <button onClick={embedReport} className="embed-report">Embed Report</button>
+
+      <DemoApp isEmbedded={isEmbedded} sampleReportConfig={sampleReportConfig} />
     </>
   )
 }
--- src/Powerbi.jsx
+++ src/Powerbi.jsx
@@ -1,26 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { models, Report, Embed, service, Page } from 'powerbi-client';
-import { IHttpPostMessageResponse } from 'http-post-message';
 import { PowerBIEmbed } from 'powerbi-client-react';
 import 'powerbi-report-authoring';
 
-import { sampleReportUrl } from '../public/constants.js';
 import './DemoApp.css';
 
-function DemoApp() {
+function DemoApp({isEmbedded, sampleReportConfig}) {
     const [report, setReport] = useState();
-    const [isEmbedded, setIsEmbedded] = useState(false);
-    const [displayMessage, setMessage] = useState(`The report is bootstrapped. Click the Embed Report button to set the access token`);
+    const [displayMessage, setMessage] = useState('');
 
     const reportClass = 'report-container';
-
-    const [sampleReportConfig, setReportConfig] = useState({
-        type: 'report',
-        embedUrl: undefined,
-        tokenType: models.TokenType.Embed,
-        accessToken: undefined,
-        settings: undefined,
-    });
 
     const [eventHandlersMap, setEventHandlersMap] = useState(new Map([
         ['loaded', () => console.log('Report has loaded')],
@@ -40,30 +28,6 @@ function DemoApp() {
         }
     }, [report]);
 
-    const embedReport = async () => {
-        console.log('Embed Report clicked');
-        const reportConfigResponse = await fetch(sampleReportUrl);
-
-        if (reportConfigResponse === null) {
-            return;
-        }
-
-        if (!reportConfigResponse?.ok) {
-            console.error(`Failed to fetch config for report. Status: ${ reportConfigResponse.status } ${ reportConfigResponse.statusText }`);
-            return;
-        }
-
-        const reportConfig = await reportConfigResponse.json();
-
-        setReportConfig({
-            ...sampleReportConfig,
-            embedUrl: reportConfig.EmbedUrl,
-            accessToken: reportConfig.EmbedToken.Token
-        });
-        setIsEmbedded(true);
-
-        setMessage('Use the buttons above to interact with the report using Power BI Client APIs.');
-    };
 
     const hideFilterPane = async () => {
         if (!report) {
@@ -141,11 +105,9 @@ function DemoApp() {
     ) : (
         <>
             <label className="display-message position">{displayMessage}</label>
-            <button onClick={embedReport} className="embed-report">Embed Report</button>
         </>
     );
 
-    const header = <div className="header">Power BI Embedded React Component Demo</div>;
 
     const reportComponent = (
         <PowerBIEmbed
@@ -159,26 +121,14 @@ function DemoApp() {
         />
     );
 
-    const footer = (
-        <div className="footer">
-            <p>This demo is powered by Power BI Embedded Analytics</p>
-            <label className="separator-pipe">|</label>
-            <img title="Power-BI" alt="PowerBI_Icon" className="footer-icon" src="./assets/PowerBI_Icon.png" />
-            <p>Explore our<a href="https://aka.ms/pbijs/" target="_blank" rel="noreferrer noopener">Playground</a></p>
-            <label className="separator-pipe">|</label>
-            <img title="GitHub" alt="GitHub_Icon" className="footer-icon" src="./assets/GitHub_Icon.png" />
-            <p>Find the<a href="https://github.com/microsoft/PowerBI-client-react" target="_blank" rel="noreferrer noopener">source code</a></p>
-        </div>
-    );
-
     return (
         <div className="container">
-            {header}
+            {/* {header} */}
             <div className="controls">
                 {controlButtons}
                 {isEmbedded ? reportComponent : null}
             </div>
-            {footer}
+            {/* {footer} */}
         </div>
     );
 }
